448154e741ca27d15429bc7e99211c1e73c910af
Zainab Zahid <108534854+zainabzahid711@users.noreply.github.com>
2024-10-14 15:50:59 +0500

    update component

--- app/components/aboutSection.tsx
+++ app/components/aboutSection.tsx
@@ -1,52 +1,53 @@
 "use client";
 
-import React from "react";
+import React, { ReactNode } from "react";
 import Button from "./button";
 import Image, { StaticImageData } from "next/image";
 
 interface AboutProps {
   image: string | StaticImageData;
   text: string;
+  buttonText: string;
+  paragraphs: ReactNode[];
+  reverse?: boolean;
 }
 
-const AboutSection: React.FC<AboutProps> = ({ image, text }) => {
+const AboutSection: React.FC<AboutProps> = ({
+  image,
+  text,
+  buttonText,
+  paragraphs,
+  reverse = false,
+}) => {
   return (
-    <section className="about-container flex md:flex-row flex-col md:pl-40 md:pr-40 pt-8 p-10 gap-12 relative group">
+    <section
+      className={`about-container flex ${
+        reverse ? "md:flex-row-reverse" : "md:flex-row"
+      } flex-col md:pl-40 md:pr-40 pt-8 p-10 gap-12 relative group`}
+    >
       <div className="absolute inset-0 z-0 bg-gray-100"></div>
+
+      <div className="md:mt-28 mt-0 fade-in z-10 relative">
+        <h1 className="text-3xl font-bold md:text-4xl mt-10 transition duration-300 ease-in-out group-hover:text-blue-400">
+          <span className="inline-block pb-2 md:pb-4">{text}</span>
+        </h1>
+
+        {paragraphs.map((para, index) => (
+          <p key={index} className="mt-3 transition duration-300 ease-in-out">
+            {para}
+          </p>
+        ))}
+
+        <div className="flex mt-8 justify-center items-center">
+          <Button text={buttonText} />
+        </div>
+      </div>
       <div className="relative max-w-full sm:max-w-lg mx-auto">
         <Image
           src={image}
           alt="About our company"
           className="w-full transition-transform duration-300 ease-in-out group-hover:shadow-2xl group-hover:shadow-black"
         />
-      </div>
-      <div className="mt-10 fade-in z-10 relative">
-        <h1 className="text-4xl md:text-5xl mt-10 transition duration-300 ease-in-out group-hover:text-blue-400">
-          <span className="inline-block md:border-b-2 md:border-orange-500 pb-2 md:pb-4">
-            {text}
-          </span>
-        </h1>
-
-        <p className="mt-16 transition duration-300 ease-in-out">
-          <span className="font-bold">Boost Collections, Cut Costs:</span> Our
-          specialized services in medical billing, data analytics, and custom
-          technical solutions are designed to increase your revenue while
-          significantly reducing operational expenses.
-        </p>
-        <p className="mt-10 transition duration-300 ease-in-out">
-          <span className="font-bold">Concierge-Style Onboarding:</span>{" "}
-          Experience seamless integration into our services with our
-          concierge-style onboarding process, ensuring a smooth transition and
-          immediate impact on your practices efficiency and bottom line.
-        </p>
-        <p className="mt-10 mb-9 transition duration-300 ease-in-out">
-          <span className="font-bold">Tech-Driven Efficiency:</span> Embrace the
-          future with our advanced systems and software, designed for seamless
-          integration and maximum impact on your practices financial health.
-        </p>
-        <div className="flex justify-center items-center">
-          <Button text="Book a Demo Call" />
-        </div>
       </div>
     </section>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,63 @@ import AboutSection from "./components/aboutSection";
 import MedicalPractice from "./components/medicalPractice";
 import Contact from "./components/contact";
 import Footer from "./components/footer";
-import aboutImage from "@/public/aboutImagee.jpg";
-import TechMeets from "@/public/original.jpg";
+import surgeryImage from "@/public/homePage/surgery.jpg";
+import microscopeImage from "@/public/homePage/microscopee.jpg";
 
 export default function Home() {
   return (
     <main className="flex flex-col">
       <NavBar />
       <HeroSection />
-      <AboutSection image={aboutImage} text="We Are Here to Help!" />
+      <AboutSection
+        image={surgeryImage}
+        text="Where care meets precision."
+        buttonText="Learn More"
+        paragraphs={[
+          <span key="optimize-revenue" className="font-bold">
+            Optimize Revenue, Minimize Costs:
+          </span>,
+          <span key="optimize-description">
+            With our expert medical billing, advanced data analytics, and
+            customized technical solutions, we streamline your operations,
+            maximizing collections while reducing overhead expenses.
+          </span>,
+          <span key="effortless-integration" className="font-bold">
+            Effortless Integration:
+          </span>,
+          <span key="integration-description">
+            Our concierge-style onboarding ensures a seamless and efficient
+            transition, allowing your practice to experience enhanced efficiency
+            and financial impact from day one.
+          </span>,
+        ]}
+        reverse={false}
+      />
+
+      <AboutSection
+        image={microscopeImage}
+        text="Where Technology Meets Care"
+        buttonText="Why choose Accrete"
+        paragraphs={[
+          <span key="transforming-healthcare" className="font-bold">
+            Transforming Healthcare with Technology:
+          </span>,
+          <span key="healthcare-description">
+            Our advanced technological solutions improve patient care and
+            streamline processes for healthcare professionals.
+          </span>,
+          <span key="innovative-data-solutions" className="font-bold">
+            Innovative Data Solutions:
+          </span>,
+          <span key="data-description">
+            Leverage cutting-edge analytics and custom software to elevate your
+            practice and enhance patient outcomes.
+          </span>,
+        ]}
+        reverse={true}
+      />
+
       <MedicalPractice />
-      <AboutSection image={TechMeets} text="Where Technology Meets Care" />
       <Contact />
       <Footer />
     </main>
